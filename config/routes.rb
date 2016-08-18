17ec016330ffd76b435121606c8040fac7ae7b06
najibu <ns.najibu@gmail.com>
2016-08-18 16:24:13 +0100

    Added chapters controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => 'registrations'}
   
-  resources :courses
+  resources :courses do
+    resoures :chapters
+  end
 
   resources :schools 
 
